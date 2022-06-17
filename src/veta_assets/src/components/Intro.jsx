056348b0b81c17564cd8c637c6f0d788e44f5565
sworks89 <mark_albitos@yahoo.com>
2022-06-18 02:54:08 +0400

    team and process update

--- src/veta_assets/src/components/Intro.jsx
+++ src/veta_assets/src/components/Intro.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import useVetaIdentity from '../contexts/VetaIdentityContext';
-import { AppBar, Toolbar, Button, Typography } from '@mui/material'
+import { AppBar, Toolbar, Button, Typography, Card, Avatar } from '@mui/material'
 import { Auth } from "./Auth"
 import { veta } from "../../../declarations/veta";
 import logo from "../images/veta-logo.svg"
@@ -10,6 +10,9 @@ import background1 from "../images/background1.jpg"
 import background2 from "../images/background2.png"
 import background3 from "../images/background3.jpg"
 import background4 from "../images/background4.jpg"
+import thanhPic from "../images/thanh.png"
+import markPic from "../images/mark.jpeg"
+import { Handshake, HowToReg, Sync, Insights, FactCheck, ConnectWithoutContact, Twitter, LinkedIn, GitHub } from "@mui/icons-material";
 
 export function Intro() {
   const { signInByICProvider, signOut, principal, client, vetaWallet } = useVetaIdentity();
@@ -42,29 +45,6 @@ export function Intro() {
   }, [])
 
   return (
-    // <>
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p style={{ fontSize: "2em", marginBottom: "0.5em" }}>Own your data</p>
-    //     <div style={{
-    //       display: "flex",
-    //       fontSize: "0.7em",
-    //       textAlign: "left",
-    //       padding: "2em",
-    //       borderRadius: "30px",
-    //       flexDirection: "column",
-    //       background: "rgb(220 218 224 / 25%)",
-    //     }}>
-    //       <div>
-    //         <Button onClick={signInByICProvider}>ADD</Button>
-    //       </div>
-    //     </div>
-    //   </header>
-    //   <Link to="/dashboard">{count}</Link>
-    //   <Button onClick={addCounter}>ADD</Button>
-    //   <Button onClick={sign}>SIGN IN</Button>
-    //   <span>welcome {principal}</span>
-    // </>
     <>
       <AppBar position="sticky"
               sx={{
@@ -75,6 +55,7 @@ export function Intro() {
                 justifyContent: 'space-between',
                 backgroundColor: 'rgba(0, 0, 0, 0.87)',
                 color: '#fff',
+                padding: '0 24px'
               }}>
         <img src={logo} className="App-logo" alt="logo" />
         <Toolbar>{/* content */}</Toolbar>
@@ -104,10 +85,10 @@ export function Intro() {
             }}></span>
 					</Typography>
       </section>
-      <section style={{ height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
+      <section style={{ padding: '24px', height: '80vh', display: 'flex', flexDirection: 'column', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
         <Typography
-          component='h2'
-          variant='h2'
+          component='h3'
+          variant='h3'
           sx={{
 
           }}>
@@ -117,25 +98,88 @@ export function Intro() {
       <section style={{ height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#fff', backgroundImage: `url(${background4})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
         {/* <img src={background1} style={{ width: '100%', position: 'absolute', top: '64px' }}/> */}
         <Typography
-						component='h2'
-						variant='h2'
+						component='h3'
+						variant='h3'
 						sx={{
               position: 'relative',
-              left: '200px',
-              fontWeight: '400'
+              left: '34%',
+              fontWeight: '400',
+              width: '500px'
 						}}>
-						BRIDGING YOUR PHYSICAL AND DIGITAL IDENTITY
+						CONNECT YOUR IDENTITY TO THE DIGITAL WORLD
 					</Typography>
       </section>
-      <section style={{ height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
+      <section style={{ padding: '24px', height: '80vh', display: 'flex', flexDirection: 'column', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
         <Typography
-          component='h2'
-          variant='h2'
+          component='h3'
+          variant='h3'
           sx={{
 
           }}>
           HOW DOES IT WORK
         </Typography>
+        <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <div style={{ height: '100%'}}></div>
+          <div style={{ height: '100%', width: '100%', maxWidth: '1200px'}}>
+            <div style={{ height: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'baseline'}}>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <HowToReg sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  1. Onboard
+                </Typography>
+                <span>User logins to wallet and onboards through KYC</span>
+              </div>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <Handshake sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  2. Connect
+                </Typography>
+                <span>User connects wallet to data platforms</span>
+              </div>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <Sync sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  3. Sync
+                </Typography>
+                <span>User manages data and syncs wallet to platform</span>
+              </div>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <ConnectWithoutContact sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  4. Share
+                </Typography>
+                <span>User shares data to verifiers and consumers</span>
+              </div>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <FactCheck sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  5. Verify
+                </Typography>
+                <span>Consumers verify data credibility through cryptographic signatures</span>
+              </div>
+              <div style={{ maxWidth: '150px', height: '210px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                <Insights sx={{ fontSize: 90 }}/>
+                <Typography
+                  component='h5'
+                  variant='h5'>
+                  6. Process
+                </Typography>
+                <span>Consumers or Verifiers process verifiable data</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ height: '100%'}}></div>
+        </div>
       </section>
       <section style={{ height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#fff', backgroundImage: `url(${background3})`, backgroundSize: 'cover' }}>
       <Typography
@@ -144,19 +188,60 @@ export function Intro() {
           sx={{
 
           }}>
-          BUILDING AN ECOSYSTEM OF TRUST
+          AN ECOSYSTEM OF TRUST
         </Typography>
       </section>
-      <section style={{ height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
+      <section style={{ padding: '24px', height: '80vh', display: 'flex', flexDirection: 'column', color: '#000', backgroundImage: `url(${background2})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
         <Typography
-          component='h2'
-          variant='h2'
+          component='h3'
+          variant='h3'
           sx={{
 
           }}>
           MEET THE TEAM
         </Typography>
+        <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '410px', backgroundColor: 'rgba(0, 0, 0, 0.87)', color: '#fff', padding: '30px', marginRight: '30px'}}>
+            <Avatar alt="Mark Albitos" src={markPic} style={{ width: '200px', height: '200px', border: '2px solid', marginBottom: '30px'}}/>
+            <Typography
+              component='h4'
+              variant='h4'>
+              Mark Albitos
+            </Typography>
+            <Typography
+              component='h5'
+              variant='h5'>
+              Founder / Developer
+            </Typography>
+            <div style={{width: '136px', display: 'flex', justifyContent: 'space-between', marginTop: '30px'}}>
+              <Twitter/>
+              <LinkedIn/>
+              <GitHub/>
+            </div>
+          </Card>
+          <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '410px', backgroundColor: 'rgba(0, 0, 0, 0.87)', color: '#fff', padding: '30px'}}>
+            <Avatar alt="Thanh Tran" src={thanhPic} style={{ width: '200px', height: '200px', border: '2px solid', marginBottom: '30px'}}/>
+            <Typography
+              component='h4'
+              variant='h4'>
+              Thanh Tran
+            </Typography>
+            <Typography
+              component='h5'
+              variant='h5'>
+              Co-Founder / Developer
+            </Typography>
+            <div style={{width: '136px', display: 'flex', justifyContent: 'space-between', marginTop: '30px'}}>
+              <Twitter/>
+              <LinkedIn/>
+              <GitHub/>
+            </div>
+          </Card>
+        </div>
       </section>
+      <footer>
+
+      </footer>
     </>
   )
 }
